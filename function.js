c6c8d55d37fce1bcce17d0af000cd97bf0f82487
Stiegelmayer Bálint <stiegelmayer.balint@burg.lo.hu>
2024-09-12 09:48:41 +0200

    Util function factorial

--- function.js
+++ function.js
@@ -1,12 +1,13 @@
 function getFirstElement(arr){
     return arr[0]
 }
-function factorial(n) {
+export function factorial(n) {
     if (n === 0 || n === 1) {
         return 1;
     }
     return n * factorial(n - 1);
 }
+console.log(`Factorial of 5: ${factorial(5)}`);
 const number = [1,2,3];
 const word = ["apple","banana","cherry"]
 console.log(`First number: ${getFirstElement(numbers)}`);
